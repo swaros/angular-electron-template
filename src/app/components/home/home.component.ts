2518c7c143d4400ec7a047f9cf64789ddf93b03e
Thomas Ziegler <thomas.zglr@googlemail.com>
2019-06-17 18:11:23 +0200

    navigation styles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @Input() navCollapsed = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public collapseNav(): void {
+    this.navCollapsed = true;
+  }
+
+  public expandNav(): void {
+    this.navCollapsed = false;
+  }
+
+  public isCollapsed(): boolean {
+    return this.navCollapsed;
+  }
+
+  public toggleNav(): void {
+    this.navCollapsed = !this.navCollapsed;
+    console.log("toggleNav", this.navCollapsed);
   }
 
 }
