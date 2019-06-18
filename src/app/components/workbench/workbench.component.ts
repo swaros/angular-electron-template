b15a6aca62927deaccbc845196a5c381255958bc
Thomas Ziegler <thomas.zglr@googlemail.com>
2019-06-18 16:14:25 +0200

    theme switch v1

--- src/app/components/workbench/workbench.component.ts
+++ src/app/components/workbench/workbench.component.ts
@@ -8,9 +8,29 @@ import { Component, OnInit, Input } from '@angular/core';
 export class WorkbenchComponent implements OnInit {
 
   @Input() navCollapsed = false;
+
+  @Input() theme: String = 'theme-light';
+
+  private themeList = ['theme-dark', 'theme-light'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public setTheme(themeName): void {
+    if (this.themeList.indexOf(themeName) > -1) {
+      this.theme = themeName;
+    }
+  }
+
+  public getAllThemes(): String[] {
+    return this.themeList;
+  }
+
+  public getTheme(): String {
+    console.log(this.theme);
+    return this.theme;
   }
 
   public collapseNav(): void {
